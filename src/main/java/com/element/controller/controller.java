ef7250279fc0560fb42a7f18eab3b8ebc37c031a
1006877 <1006877@users.noreply.github.com>
2017-01-14 15:45:53 +0530

    Update controller.java

--- src/main/java/com/element/controller/controller.java
+++ src/main/java/com/element/controller/controller.java
@@ -1,1 +1,42 @@
-https://github.com/1006877/LogsManagement
+package com.element.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.element.edb.entity.DealEntity;
+import com.element.service.CustomerService;
+
+@RestController
+@RequestMapping("tcs")
+public class controller {
+
+	@Autowired
+	@Qualifier("customerServiceImpl")
+	private CustomerService customerService;
+	
+	@RequestMapping(value="/getcustomer" ,method=RequestMethod.GET)
+    public  List<DealEntity> getCustomer(){
+    	
+    	List<DealEntity> custList = customerService.getCustomer();
+    	
+    	
+    	
+		return custList;
+		
+	}
+   
+    
+    @RequestMapping(value="/getcustomerByName" ,method=RequestMethod.GET)
+    public DealEntity getCustomerByName(@RequestParam String name){
+		return customerService.getCustomerByName(name);
+		
+	}
+   
+	
+}
