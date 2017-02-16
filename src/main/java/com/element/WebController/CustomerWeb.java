eccc41d185d4990201e7636a4faa025e126bd32c
1006877 <1006877@users.noreply.github.com>
2017-02-16 15:42:40 +0530

    Update CustomerWeb.java

--- src/main/java/com/element/WebController/CustomerWeb.java
+++ src/main/java/com/element/WebController/CustomerWeb.java
@@ -46,7 +46,7 @@ public class CustomerWeb {
     public  String home(){
   
 	logger.debug("getWelcome is executed!");		
-	logger.error("This is Home Page");
+	logger.error("This is my Page");
 		
 	
 	
