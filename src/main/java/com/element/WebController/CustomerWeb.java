3f7400baba06b959826bd86b67f0a68f591aeec8
1006877 <1006877@users.noreply.github.com>
2017-02-24 10:47:51 -0800

    Update CustomerWeb.java

--- src/main/java/com/element/WebController/CustomerWeb.java
+++ src/main/java/com/element/WebController/CustomerWeb.java
@@ -48,7 +48,7 @@ public class CustomerWeb {
 		
 		
 		
-                logger.debug("mostWelcome!");
+                logger.debug("mostWelcomeVishal!");
 	        logger.debug("getWelcome!");
 	        System.out.println("Vishal");
 		logger.error("This is Welcome page");
