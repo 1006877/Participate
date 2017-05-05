1acf8798eee753287d0c54e9eafa6a2c20d582b8
1006877 <1006877@users.noreply.github.com>
2017-05-05 13:47:26 +0530

    Update CustomerWeb.java

--- src/main/java/com/element/WebController/CustomerWeb.java
+++ src/main/java/com/element/WebController/CustomerWeb.java
@@ -47,7 +47,7 @@ public class CustomerWeb {
 		
 		logger.debug("Vishal!");
 		
-                logger.debug("Vishal!");
+                logger.debug("Vishal Tiwari !");
 	        logger.debug("getWelcome bhaag bhai!");
 
 	
